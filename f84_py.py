e2c159aac9cf30b7d6a37cc7ad12b3d734ca406f
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2023-11-17 05:22:32 +0700

    Add files via upload

---
+++ f84_py.py
@@ -0,0 +1,21 @@
+# F84 simple launcher
+# by fahmiyufrizal@2023
+
+import os
+import subprocess
+from os import path
+from sys import exit
+import asyncio
+import click
+import pathlib
+
+# parameters
+version = 'v1.0'
+titlewindow = 'fahmiyufrizal@2023 [github.com/fahmiyufrizal]'
+F84FN = (r'Farlight84_NetCafe_Launcher.exe')
+F84LN = (r'Farlight84Launcher.exe')
+dp0 = os.getcwd()
+
+#csonored
+if __name__ == '__main__':
+    cli()
